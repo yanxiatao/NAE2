ebb140835113e3651ea3c0a83914f7e4ada008cc
yanxiatao <63758728+yanxiatao@users.noreply.github.com>
2026-02-16 13:56:58 +0800

    Update MixinDualityInterface.java

--- src/main/java/co/neeve/nae2/mixin/upgrades/gregcircuit/MixinDualityInterface.java
+++ src/main/java/co/neeve/nae2/mixin/upgrades/gregcircuit/MixinDualityInterface.java
@@ -27,7 +27,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Set;
 import java.util.WeakHashMap;
 
-@Mixin(value = DualityInterface.class, remap = true)
+@Mixin(value = DualityInterface.class, remap = false)
 public abstract class MixinDualityInterface {
 	@Unique
 	private WeakHashMap<ICraftingPatternDetails, Integer> nae2$cachedCircuitValues = null;
